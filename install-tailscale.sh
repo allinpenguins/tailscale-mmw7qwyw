2f09ec299736efc6a16288d27a7495d58f8a1f54
allinpenguins <allinpenguins@proton.me>
2026-03-19 01:07:05 +0800

    Update install-tailscale.sh

--- install-tailscale.sh
+++ install-tailscale.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -x
-TAILSCALE_VERSION=${TAILSCALE_VERSION:-1.92.3}
+TAILSCALE_VERSION=${TAILSCALE_VERSION:-1.96.2}
 TS_FILE=tailscale_${TAILSCALE_VERSION}_amd64.tgz
 wget -q "https://pkgs.tailscale.com/stable/${TS_FILE}" && tar xzf "${TS_FILE}" --strip-components=1
 cp -r tailscale tailscaled /render/
